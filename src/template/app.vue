7d932cacdad51eb0c9eb342037126a9ca8aed947
卢程 <lucheng@qijiayoudao.com>
2018-08-08 23:42:26 +0800

    添加vue-loader babel-laoder去解析vue组件

---
+++ src/template/app.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="count" @click="count++">{{count}}</div>
+        <qr-code text="hello world"></qr-code>
+    </div>
+</template>
+
+<script>
+  import qrCode from 'vue-qrcode-component'
+
+  export default {
+    data () {
+      return {
+        count: 0
+      }
+    },
+    components: {
+      'qr-code': qrCode
+    }
+  }
+</script>
+
+<style>
+    .count {
+        color: red;
+    }
+</style>
--- webpack.base.js
+++ webpack.base.js
@@ -2,11 +2,14 @@ const path = require('path')
 const webpack = require('webpack')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const cleanWebpackPlugin = require('clean-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+//
+// const entries = {
+//   index: './src/index.js',
+//   app: './src/app.js'
+// }
 
-const entries = {
-  index: './src/index.js',
-  app: './src/app.js'
-}
+const entries = './src/main.js'
 
 
 
@@ -17,16 +20,42 @@ const baseConfig = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
   },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    symlinks: false
+  },
   module: {
     rules: [
       {
-        test: '/\.css$/',
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader?cacheDirectory',
+        exclude: file => (
+          /node_modules/.test(file) &&
+          !/\.vue\.js/.test(file)
+        )
+      },
+      {
+        test: /\.css$/,
         use: ['style-loader', 'css-loader']
       }
     ]
   },
   plugins: [
-    new cleanWebpackPlugin(['dist/*'])
+    new VueLoaderPlugin(),
+    new cleanWebpackPlugin(['dist/*']),
+    new htmlWebpackPlugin({
+      title: '测试',
+      template: 'index.html',
+      filename:  'index.html',
+      //chunks: [entry, 'mainifest', 'vendor']
+    })
     //new webpack.HashedModuleIdsPlugin()  // 建议在生产环境使用 nameModulePlugin() 建议在开发环境使用
   ],
   optimization: {
@@ -42,10 +71,8 @@ const baseConfig = {
               ) === 0
             )
           },
-          name: 'app',
-          chunks: () => {
-            return true
-          },
+          name: 'vendor',
+          chunks: 'all',
           minSize: 0,
           reuseExistingChunk: true
         }
@@ -57,15 +84,16 @@ const baseConfig = {
   }
 }
 
-for (let entry in entries) {
-  if (entries.hasOwnProperty(entry)) {
-    baseConfig.plugins.push(new htmlWebpackPlugin({
-      title: entry,
-      filename: entry + '.html',
-      chunks: [entry, 'mainifest', 'vendor']
-    }))
-  }
-}
+// for (let entry in entries) {
+//   if (entries.hasOwnProperty(entry)) {
+//     baseConfig.plugins.push(new htmlWebpackPlugin({
+//       title: entry,
+//       template: 'index.html',
+//       filename: entry + '.html',
+//       chunks: [entry, 'mainifest', 'vendor']
+//     }))
+//   }
+// }
 
 
 module.exports = baseConfig
